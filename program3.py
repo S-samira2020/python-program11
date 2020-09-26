49d63da204588acd929aec4f1097ea061060e08d
Samira <54632872+S-samira2020@users.noreply.github.com>
2020-09-26 09:48:23 +0600

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,18 @@
+
+
+'''r_topping = ' mushrooms '
+
+if r_topping  != ' cauliflower ':
+    print("Hold the cauliflower!")
+'''
+
+
+
+#   checking wheather a value is not in list
+
+b_users = [' carolina', ' andrew', ' david']
+user = ' marie'
+
+if user  not in b_users:
+    print()
+    print(user.title() + " , you can post a response if you wish.")
